c7cad9d6f11c7f1834d5598b8412b3226bc47d60
Ben Rondeau <benrondeau@users.noreply.github.com>
2016-03-30 08:01:36 -0500

    Fixed error and removed comments

--- server.js
+++ server.js
@@ -2,23 +2,21 @@
 var https = require('https');
 var querystring = require('querystring');
 
+// Push Notification Module
 module.exports = function (credentials, notification){
 
-  // Serialize notification into a query string. See docs @ https://nodejs.org/api/querystring.html#querystring_querystring_stringify_obj_sep_eq_options, Node.js v0.12.1
   var postData = querystring.stringify(notification);
 
-  // Object that defines http.request(). See docs @ https://nodejs.org/api/http.html#http_http_request_options_callback, Node.js v0.12.1
   var options = {
-  	hostname: 'api.ionic.io',
+	hostname: 'api.ionic.io',
 	path: '/push/notifications',
-    	method: 'POST',
-    	headers: {
-		"Content-Type" : "application/json",
-  		"Authorization": "Bearer + credentials.IonicApplicationAPItoken",
-      	}
+	method: 'POST',
+	headers: {
+	  "Content-Type" : "application/json",
+	  "Authorization": "Bearer " + credentials.IonicApplicationAPItoken,
+	}
   };
 
-  // HTTP POST request. See docs @ https://nodejs.org/api/http.html#http_http_request_options_callback, Node.js v0.12.1
   var req = https.request(options, function(res) {
     console.log('STATUS: ' + res.statusCode);
     console.log('HEADERS: ' + JSON.stringify(res.headers));
@@ -28,12 +26,10 @@ module.exports = function (credentials, notification){
     });
   });
 
-  // Error handling.
   req.on('error', function(e) {
     console.log('problem with request: ' + e.message);
   });
 
-  // Wite data to request body
   req.write(JSON.stringify(notification));
   req.end();
 
